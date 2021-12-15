d44c34cba84a12d73b14963d506a1e31b3b8539d
Exem <chernyakov.ghost@mail.ru>
2021-12-15 21:32:16 +0500

    fixed

--- src/components/queueMemberItem.vue
+++ src/components/queueMemberItem.vue
@@ -11,7 +11,7 @@
 
 <script lang="ts">
 import queueMember from '@/classes/queueMember'
-import { defineComponent, onMounted } from 'vue'
+import { defineComponent, onMounted, watch } from 'vue'
 import { useStore } from 'vuex'
 import { key } from '@/store'
 export default defineComponent({
@@ -23,45 +23,29 @@ export default defineComponent({
         const store = useStore(key)
         let timeModifier: number = store.state.queueSpeed;
 
+        const indexchanged = () => {
+            if(props.queueIndex === 0) {
+                store.state.modalTimeouts.push(setTimeout(() => {
+                    store.state.needModal = true //показываем модалку
+                    setTimeout(() => {
+                        store.state.needModal = false
+                        store.commit('REMOVE_FROM_QUEUE') //удаляем из очереди
+                    }, 5000);
+                }, timeModifier))
+            }
+        }
+
+        watch(props, indexchanged)
+
         onMounted(() => {
             if(props.queueIndex === 0){ //первому в очереди дается в 10 раз меньше времени
-                let today = new Date();
-                today.setUTCSeconds(today.getSeconds() + 5)
-                today.setMilliseconds(today.getMilliseconds() + timeModifier/10)
-
-                store.state.lastQueueMemberDeployTime = today //так как этот элемент первый, устанавливаем его время удаления в стор без сравнения
-
                 store.state.modalTimeouts.push(setTimeout(() => {
-                    if(store.state.modalTimeouts) { //был баг с изменением сокорости очереди, пришлось проверку добавить
-                        store.state.needModal = true //вызываем модальное окно с приглашением
-
-                        setTimeout(() => {
-                            store.state.needModal = false //убираем модалку и удаляем элемент из очереди
-                            store.commit('REMOVE_FROM_QUEUE')
-                        }, 5000);
-                    }
-
-
+                    store.state.needModal = true //вызываем модальное окно с приглашением
+                    setTimeout(() => {
+                        store.state.needModal = false //убираем модалку и удаляем элемент из очереди
+                        store.commit('REMOVE_FROM_QUEUE')
+                    }, 5000);
                 }, timeModifier/10))
-            }
-            else {
-                let lastDeployed = store.state.lastQueueMemberDeployTime
-                let nowTime = new Date()
-
-                let diff = Math.abs((lastDeployed.valueOf() - nowTime.valueOf())/1000) //узнаем сколько времени осталось до деплоя последнего в очереди
-                nowTime.setSeconds(nowTime.getSeconds() + diff + timeModifier/1000 + 5) //прибавляем с учетом модалки на 5 сек
-                store.state.lastQueueMemberDeployTime = nowTime //устанавливаем в стор время предпологаемого удаления из очереди
-
-                store.state.modalTimeouts.push(setTimeout(() => {
-                    if(store.state.modalTimeouts) { //был баг с изменением сокорости очереди, пришлось проверку добавить
-                        store.state.needModal = true //показываем модалку
-
-                        setTimeout(() => {
-                            store.state.needModal = false
-                            store.commit('REMOVE_FROM_QUEUE') //удаляем из очереди
-                        }, 5000);
-                    }
-                }, 1000*diff + timeModifier))
             }
         })
     },
